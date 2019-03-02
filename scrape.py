dc520408c1916b2199cdef7dd06654e4592d56ef
astroboy1 <omorim@hawaii.edu>
2019-03-01 20:41:20 -1000

    parsing tournaments

--- scrape.py
+++ scrape.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
-import random
 import pandas as pd
+import re
 
 
 class User:
@@ -15,9 +15,9 @@ class User:
 
 
 class UserEvents:
-    """Dimensions table"""
+    """Dimension table"""
     def __init__(self, event_id, user_id, date, event_name, reg_before, reg_after, quick_before, quick_after,
-                 blitz_before, blitz_after):
+                 blitz_before, blitz_after, section, state):
         self.event_id = event_id
         self.user_id = user_id
         self.date = date
@@ -28,6 +28,8 @@ class UserEvents:
         self.quick_after = quick_after
         self.blitz_before = blitz_before
         self.blitz_after = blitz_after
+        self.section = section
+        self.state = state
 
 
 def simple_get(url):
@@ -82,18 +84,49 @@ def get_user_info(parsed_html, user_id, users_list):
     return 0
 
 
-def get_event_ratings(parsed_html, user_events_list):
+def rating_helper(rating_string):
+    """Gets the before and after rating for one instance with simple processing"""
+    rating_split = rating_string.split("=>")
+    first, second = rating_split[0].strip(), rating_split[1].strip()
+    return first, second
+
+
+def get_event_ratings(parsed_html, user_id, user_events_list):
     """Takes in the html from beautiful soup and gets the tournament event id, dates, and ratings"""
     table = parsed_html.find('table', {'valign': 'top', 'width': '960', 'cellspacing': '0', 'cellpadding': '4', 'border': '1'})
     if table:
         dates = table.find_all("td", {'width': '120'})
         tournaments = table.find_all("td", {'width': '350'})
         ratings = table.find_all("td", {'width': '160'})
+        rating_i = 0
         for d, t in zip(dates, tournaments):
-            d_s = str(d)
-            parsed_date = d_s[16:26]
-            parsed_eventID = d_s[38:-14]
+            d_s = d.getText()
+            parsed_date = d_s[:10]
+            parsed_event_id = d_s[10:]
             t_text = t.getText()
+            # Matches anything that starts with ( and ends with ), .* says there can be zero or more of any characters
+            match = re.search("\(.*\)", t_text)
+            start, end = match.span()
+            section = t_text[end:]
+            state = t_text[start + 1:end - 1]
+            tournament_name = t_text[:start - 1]
+            reg_ratings = ratings[rating_i].getText()
+            quick_ratings = ratings[rating_i + 1].getText()
+            blitz_ratings = ratings[rating_i + 2].getText()
+            rating_i += 3
+            reg_rating_before, reg_rating_after, quick_rating_before, quick_rating_after, blitz_rating_before, \
+                blitz_rating_after = None, None, None, None, None, None
+            if "=>" in reg_ratings:
+                reg_rating_before, reg_rating_after = rating_helper(reg_ratings)
+            if "=>" in quick_ratings:
+                quick_rating_before, quick_rating_after = rating_helper(quick_ratings)
+            if "=>" in blitz_ratings:
+                blitz_rating_before, blitz_rating_after = rating_helper(blitz_ratings)
+            ue = UserEvents(parsed_event_id, user_id, parsed_date, tournament_name, reg_rating_before, reg_rating_after,
+                            quick_rating_after, quick_rating_after, blitz_rating_before, blitz_rating_after, section,
+                            state)
+            user_events_list.append(ue)
+        return 0
     else:
         return -1
 
@@ -103,8 +136,8 @@ if __name__ == "__main__":
     user_events_df = pd.DataFrame()
     user_events = []
     users = []
-    '''TODO: Store the data in a database, try google colab, just analyze tournament results between players, predict chess player that will
-    win a tournament, getText before indexing'''
+    '''TODO: Store the data in a sqlite database, try Google Colab, some testing, loop through each tournament page'''
+    # Finish get_event_ratings function
     for i in range(1, 2):
         uid = str(1243502) + str(i)
         page = "http://www.uschess.org/msa/MbrDtlMain.php?" + uid
@@ -117,7 +150,6 @@ if __name__ == "__main__":
         raw_html = simple_get(page)
         html = BeautifulSoup(raw_html, 'html.parser')
         get_user_info(html, uid, users)
-        """TODO: create event class, and use the correct page for getting"""
         raw_html = simple_get(rating_page)
         html = BeautifulSoup(raw_html, 'html.parser')
-        get_event_ratings(html, user_events)
+        get_event_ratings(html, uid, user_events)
